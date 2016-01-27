eb3c9702082ab7b1da48bfd31e9c9e707414265a
adug3 <m_fan@geishatokyo.com>
2016-01-27 21:11:35 +0900

    add support for node of matrix transform, symbol type attribute

--- fxg2svg.py
+++ fxg2svg.py
@@ -15,28 +15,67 @@ class FxgToSvg:
         self.name_key = []
         self.symbols = dict()
         self.origin_name = dict()
+        self.flm_ns = '{http://ns.adobe.com/flame/2008}'
+        self.instance_types = ['no', 'graphic']
+
+    def flm_full_name(self, name):
+        return self.flm_ns + name
 
     def parse_defines(self):
         for def_node in self.fxg_root.iter():
             tag = self.remove_namespace(def_node.tag)
             if tag == 'Definition':
+                symbol_type = def_node.attrib.get(self.flm_full_name('symbolType'), '0')
                 name = def_node.attrib['name']
-                self.symbols[name] = list(def_node)
-                if '{http://ns.adobe.com/flame/2008}originalName' in def_node.attrib:
-                    self.origin_name[name] = def_node.attrib['{http://ns.adobe.com/flame/2008}originalName']
-                else:
-                    self.origin_name[name] = None
-                print('save %s' % name)
 
-    def parse_color(self, path_node):
-        colors = []
-        fill_node = path_node.find('./*')
-        for node in fill_node.findall('./*'):
-            if self.remove_namespace(node.tag) == 'SolidColor':
-                colors.append(node.attrib['color'])
+                self.symbols[(name, symbol_type)] = list(def_node)
+                if self.flm_full_name('originalName') in def_node.attrib:
+                    self.origin_name[(name, symbol_type)] = def_node.attrib[self.flm_full_name('originalName')]
+                else:
+                    self.origin_name[(name, symbol_type)] = None
+                self.name_key.append(name)
+                #print('save %s %s' % (name, symbol_type))
+
+    def parse_path_style(self, path_node):
+        style = dict()
+        for style_node in path_node.findall('./*'):
+            tag = self.remove_namespace(style_node.tag)
+            if tag == 'fill':
+                for node in style_node.findall('./*'):
+                    if self.remove_namespace(node.tag) == 'SolidColor':
+                        if 'fill' not in style:
+                            style['fill'] = (node.attrib['color'])
+                        else:
+                            raise Exception
+                    else:
+                        print(node)
+                        raise Exception
+            elif tag == 'transform':
+                transform_node = style_node.find('./*')
+                if self.remove_namespace(transform_node.tag) == 'Transform':
+                    transform_item = transform_node.find('./*')
+                    transform_tag = self.remove_namespace(transform_item.tag)
+                    if transform_tag == 'matrix':
+                        if 'transform' not in style:
+                            matrix_node = transform_item.find('./*')
+                            style['transform'] = 'matrix(%s,%s,%s,%s,%s,%s)' % (
+                                matrix_node.attrib['a'], matrix_node.attrib['b'], matrix_node.attrib['c'],
+                                matrix_node.attrib['d'], matrix_node.attrib['tx'], matrix_node.attrib['ty'])
+                        else:
+                            raise Exception
+                    elif transform_tag == 'colorTransform':
+                        print('colorTransform')
+                    else:
+                        print(transform_item)
+                        raise Exception
+                else:
+                    print(transform_node)
+                    raise Exception
             else:
+                print(tag)
                 raise Exception
-        return colors
+
+        return style
 
     @staticmethod
     def parse_attrib(attribute):
@@ -69,7 +108,7 @@ class FxgToSvg:
             return tag
 
     def parse(self, fxg_node, svg_node, id=None):
-        print('parse %s %s' % (self.remove_namespace(fxg_node.tag), svg_node.tag))
+        #print('parse %s %s' % (self.remove_namespace(fxg_node.tag), svg_node.tag))
         for fxg_child in fxg_node.findall("./*"):
             tag = self.remove_namespace(fxg_child.tag)
             if tag in ['Graphic']:
@@ -98,24 +137,28 @@ class FxgToSvg:
                 svg_attrib['d'] = path_string
 
                 # Parse color data
-                colors = self.parse_color(fxg_child)
-                if len(colors) == 1:
-                    svg_attrib['fill'] = colors[0]
-                elif len(colors) > 1:
-                    raise Exception
+                style = self.parse_path_style(fxg_child)
+                if 'fill' in style:
+                    svg_attrib['fill'] = style['fill']
+                if 'transform' in style:
+                    transform_string = svg_attrib.get('transform', '') + style['transform']
+                    svg_attrib['transform'] = transform_string
 
                 svg_child = ET.Element('path', svg_attrib)
                 svg_node.append(svg_child)
-            elif tag in self.symbols:
-                print("parse %s" % tag)
+            elif tag in self.name_key:
                 svg_attrib = dict()
                 svg_attrib.update(self.parse_attrib(fxg_child.attrib))
-                if tag not in self.symbols:
+                instance_type = fxg_child.get(self.flm_full_name('instanceType'), 'no')
+                symbol_type = str(self.instance_types.index(instance_type))
+
+                #print("tag %s %s %s" % (tag, instance_type, symbol_type))
+                if (tag, symbol_type) not in self.symbols:
                     print(tag)
                     raise Exception
 
-                symbol_node = self.symbols[tag]
-                print("len of symbol_node %d" % len(symbol_node))
+                symbol_node = self.symbols[(tag, symbol_type)]
+                #print("len of symbol_node %d" % len(symbol_node))
                 if len(symbol_node) > 1 or len(svg_attrib) > 0:
                     svg_child = ET.Element('g', svg_attrib)
                     for symbol in symbol_node:
@@ -125,8 +168,8 @@ class FxgToSvg:
                     self.parse(symbol_node[0], temp_root)
                     svg_child = list(temp_root)[0]
                     svg_node.append(svg_child)
-                if self.origin_name[tag]:
-                    svg_child.attrib['class'] = self.origin_name[tag].replace(' ', '')
+                if self.origin_name[(tag, symbol_type)]:
+                    svg_child.attrib['class'] = self.origin_name[(tag, symbol_type)].replace(' ', '')
                     print(svg_child.attrib['class'])
                 svg_node.append(svg_child)
             elif tag in ['Library', 'Definition']:
